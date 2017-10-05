67687bcbddac2d4c8ac3131b574802039270f3b1
Kristine <kristinesimon31@gmail.com>
2017-10-05 16:52:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kristine/simon_kistine_ics115_oct5new/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kristine.simon_kistine_ics115_oct5new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
